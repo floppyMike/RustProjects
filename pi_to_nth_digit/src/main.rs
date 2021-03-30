bf73d18d629892f52ab8957bf505ed9a59db6f3d
Michaellu <mchblch@protonmail.com>
2021-03-30 17:10:12 +0200

    pi till number

--- pi_to_nth_digit/src/main.rs
+++ pi_to_nth_digit/src/main.rs
@@ -1,3 +1,30 @@
+use std::slice;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    const PI: &'static str = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706";
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("pi_to_nth_digit <num>\nShow decimals of a string till 100.");
+        return;
+    }
+
+    let num = match usize::from_str_radix(&args[1], 10) {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Unrecognized number.");
+            return;
+        }
+    };
+
+    if num > PI.len() {
+        println!("Decimal is up to 100.");
+        return;
+    }
+
+    let slice = unsafe { str::from_utf8(slice::from_raw_parts(PI.as_ptr(), num)).unwrap() };
+
+    println!("{}", slice);
 }
